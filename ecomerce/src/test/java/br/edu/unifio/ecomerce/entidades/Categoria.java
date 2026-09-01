3ede1f9650f9e0c8b898b81f2d3b6cdd95b14e53
Luis Felipi Ferreira <ferreiraluisfelipi7@gmail.com>
2026-09-01 00:40:32 +0000

    1

---
+++ ecomerce/src/test/java/br/edu/unifio/ecomerce/entidades/Categoria.java
@@ -0,0 +1,12 @@
+package br.edu.unifio.ecomerce.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Categoria {
+    private Short id;
+    private String nome;
+    private String descricao;
+}
